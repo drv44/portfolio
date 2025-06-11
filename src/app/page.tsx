68e3b4b7a1bba431c70d98979a3cabf16f494da6
drv44 <dhruvsarkar960@gmail.com>
2025-06-11 16:09:45 +0530

    klpop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 import Header from "@/components/header"
 import HeroSection from "@/components/hero-section"
-import AboutSection from "@/components/about-section"
-import ProjectsSection from "@/components/projects-section"
+// import AboutSection from "@/components/about-section"
+// import ProjectsSection from "@/components/projects-section"
 import { SimpleAboutSection } from "@/components/simple";
 // import ExperienceSection from "@/components/experience-section"
 import ContactSection from "@/components/contact-section"
